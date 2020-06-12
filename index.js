0871cbee8ea20c788d5eac78cf1687c133a422cc
Samara Grant <samara.amanda01@gmail.com>
2020-06-12 23:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(sentence) {
-  if (sentence === "I love you, Grandma" ) {
+  if (sentence === "I love you, Grandma") {
     return 'I love you, too'
   } else if (sentence === whisper(sentence)) {
     return "I can't hear you!"
