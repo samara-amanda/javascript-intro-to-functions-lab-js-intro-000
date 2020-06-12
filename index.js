bde4df0a96ee0f23b9b8f1909448f38e58d18105
Samara Grant <samara.amanda01@gmail.com>
2020-06-12 23:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(sentence) {
-  if (sentence === "I love you, grandma" ) {
+  if (sentence === "I love you, Grandma" ) {
     return 'I love you, too'
   } else if (sentence === whisper(sentence)) {
-    return "I can't hear you"
-  } else if () {
-
+    return "I can't hear you!"
+  } else if (sentence === shout(sentence)) {
+    return 'YES INDEED!'
   }
 }
